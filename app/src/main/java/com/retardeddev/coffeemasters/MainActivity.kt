bca3b5ffac93f33b13b0af1d65fd965babe7ba1b
KaSaNaa <pasindudeshitha123@gmail.com>
2024-03-14 21:25:40 +0530

    minor changes

--- app/src/main/java/com/retardeddev/coffeemasters/MainActivity.kt
+++ app/src/main/java/com/retardeddev/coffeemasters/MainActivity.kt
@@ -3,13 +3,18 @@ package com.retardeddev.coffeemasters
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.retardeddev.coffeemasters.ui.theme.CoffeeMastersTheme
 
 class MainActivity : ComponentActivity() {
@@ -27,15 +32,25 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Preview(showBackground = true, widthDp = 200)
 @Preview(showBackground = true)
 @Composable
 fun FirstComposable() {
-    Text(text = "Hello JetPack Compose")
+    Row {
+        Text(text = "Hello JetPack Compose", modifier = Modifier
+            .background(Color.Yellow)
+            .padding(16.dp)
+        )
+        Text(text = "Other Text", modifier = Modifier
+            .background(Color.Green)
+            .padding(16.dp)
+        )
+    }
+
+    Text(text = "Other Text")
 }
 
 @Preview(showBackground = true)
 @Composable
 fun SecondComposable() {
-    Text(text = "Hello JetPack Compose")
+    Text(text = "Hello JetPack")
 }
