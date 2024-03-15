984e0f0410c2720c21ba250b7a789abcbfdf0796
KaSaNaa <pasindudeshitha123@gmail.com>
2024-03-15 23:31:28 +0530

    created NavBar

--- app/src/main/java/com/retardeddev/coffeemasters/App.kt
+++ app/src/main/java/com/retardeddev/coffeemasters/App.kt
@@ -6,9 +6,12 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
@@ -30,6 +33,9 @@ fun AppPreview() {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun App() {
+    var currentPage = remember{
+        mutableStateOf(Routes.MenuPage.route)
+    }
     Scaffold(
         topBar = {
             TopAppBar(
@@ -42,9 +48,20 @@ fun App() {
                 ),
             )
         },
-        bottomBar = {},
+        bottomBar = {
+            NavBar(
+                selectedRoute = currentPage.value,
+                onChange = { newRoute ->
+                currentPage.value = newRoute
+            })
+        },
         content = {
-            OffersPage()
+            when (currentPage.value) {
+                Routes.MenuPage.route -> Text("Menu")
+                Routes.offersPage.route -> OffersPage()
+                Routes.OrderPage.route -> Text("Order")
+                Routes.InfoPage.route -> Text("Info")
+            }
         }
     )
 }
